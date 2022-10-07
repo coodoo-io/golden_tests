1c9c2b1df870b1b54475fbf86055e1b885e807fb
Markus Kühle <markus.kuehle@coodoo.de>
2022-10-07 15:18:19 +0200

    Struktur und Makefile

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -8,7 +8,7 @@
 import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_test/main.dart';
+import 'package:golden_test/app.dart';
 
 void main() {
   group('CounterPage', () {
